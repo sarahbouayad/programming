639616cebb0d93b678b59b5fe178ba4db684ebad
Sarah Bouayad <112355619+sarebouayad@users.noreply.github.com>
2023-05-24 20:08:36 -0400

    added inline widget

--- calendly.js
+++ calendly.js
@@ -2,3 +2,13 @@
 
 <script type="text/javascript" src="https://calendly.com/assets/external/calendly-squarespace.js"></script>
 
+
+
+
+
+// inline code widget 
+
+// Calendly inline widget begin
+<div class="calendly-inline-widget" data-url="https://calendly.com/businessname/30min" style="min-width:320px;height:700px;"></div>
+<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+// Calendly inline widget end
